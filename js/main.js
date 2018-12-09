76e932e605f0a11c11a94e3082822c4f29c2a095
Ilpo Loikkanen <l4538@student.jamk.fi>
2018-12-09 14:51:32 +0200

    added will smith fortnite

--- js/main.js
+++ js/main.js
@@ -87,6 +87,21 @@ b.on('message', message => {
                 }
             
             break;
+            case 'will':
+                if(message.member.voiceChannel) {
+                    message.member.voiceChannel.join()
+                    .then(connection => {
+                        const dispatcher = connection.playFile('misc/audio/fortnite-will-smith.mp4', {passes: 3, volume: 0.4});
+                        dispatcher.on("end", end => {
+                            message.member.voiceChannel.leave();
+                        });
+                    })
+                    .catch(console.log);
+                } else {
+                    message.reply('You are not in a voice channel I could join. >:(')
+                }
+            
+            break;
             //istun poikittain
             case 'istun-poikittain':
                 if(message.member.voiceChannel) {
